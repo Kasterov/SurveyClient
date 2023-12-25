88770d85a0d2be144aa46783a79490b8c4e8ce6f
Slava <kasterov.v@gmail.com>
2023-12-25 14:04:29 +0200

    SignUp view added

--- src/views/pages/auth/SignUp.vue
+++ src/views/pages/auth/SignUp.vue
@@ -3,7 +3,9 @@ import { useLayout } from '@/layout/composables/layout';
 import { ref, computed } from 'vue';
 import AppConfig from '@/layout/AppConfig.vue';
 import { repositoryUser } from '@/repository/repositoryUser';
+import { useRouter } from 'vue-router'
 
+const router = useRouter();
 const { registerUser } = repositoryUser();
 const { layoutConfig } = useLayout();
 
@@ -11,7 +13,10 @@ const userName = ref('');
 const email = ref('');
 const password = ref('');
 const dateOfBirthValue = ref(null);
-const checked = ref(false);
+
+let invalidUserName = ref('');
+let invalidPassword = ref('');
+let invalidEmail = ref('');
 
 const createUserDTO = async () => {
 
@@ -23,7 +28,30 @@ const createUserDTO = async () => {
         DateOfBirth: dateOfBirthValue.value
     }
 
-    await registerUser(createUserDTO);
+    if(createUserDTO.Name == ''){
+        invalidUserName.value = 'p-invalid';
+    } else {
+        invalidUserName.value = '';
+    }
+
+    if(createUserDTO.Password == ''){
+        invalidPassword.value = 'p-invalid';
+    } else {
+        invalidPassword.value = '';
+    }
+
+    if(createUserDTO.Email == ''){
+        invalidEmail.value = 'p-invalid';
+    } else {
+        invalidEmail.value = '';
+    }
+
+    if(createUserDTO.Email == '' || createUserDTO.Password == '' || createUserDTO.Name == ''){
+        return;
+    }
+
+    // await registerUser(createUserDTO);
+    router.push({ path: '/uikit/poolhub' });
 } 
 
 const logoUrl = computed(() => {
@@ -46,13 +74,13 @@ const logoUrl = computed(() => {
 
                     <div>
                         <label for="userName1" class="block text-900 text-xl font-medium mb-2">User name</label>
-                        <InputText id="userName1" type="text" placeholder="User name" class="w-full md:w-30rem mb-5" style="padding: 1rem" v-model="userName" />
+                        <InputText id="userName1" type="text" placeholder="User name" :onClick="() => {invalidUserName = ''}" :class="invalidUserName" class="w-full md:w-30rem mb-5" style="padding: 1rem" v-model="userName" />
 
                         <label for="email1" class="block text-900 text-xl font-medium mb-2">Email</label>
-                        <InputText id="email1" type="text" placeholder="Email address" class="w-full md:w-30rem mb-5" style="padding: 1rem" v-model="email" />
+                        <InputText id="email1" type="text" placeholder="Email address" :onClick="() => {invalidEmail = ''}" :class="invalidEmail" class="w-full md:w-30rem mb-5" style="padding: 1rem" v-model="email" />
 
                         <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
-                        <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" class="w-full mb-5" inputClass="w-full" :inputStyle="{ padding: '1rem' }"></Password>
+                        <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" :onClick="() => {invalidPassword = ''}" :class="invalidPassword" class="w-full mb-5" inputClass="w-full" :inputStyle="{ padding: '1rem' }"></Password>
 
                         <label for="dateOfBirth" class="block text-900 font-medium text-xl mb-2">Date of birth</label>
                         <Calendar class="w-full mb-5" :showIcon="true" :showButtonBar="true" v-model="dateOfBirthValue"></Calendar>
