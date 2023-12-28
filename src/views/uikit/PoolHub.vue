fb37435949ccc81388391f0c21df8c7cc663cfe8
Slava <kasterov.v@gmail.com>
2023-12-28 12:26:42 +0200

    Update PoolHub.vue

--- src/views/uikit/PoolHub.vue
+++ src/views/uikit/PoolHub.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import PoolhubService from '@/service/PoolhubService';
+import { repositoryPost } from '@/repository/repositoryPost';
+import { repositoryVote } from '@/repository/repositoryVote';
+
+const { getPoolOptionListByPostId } = repositoryPost();
+const { createVoteList } = repositoryVote();
 
 let documentStyle = getComputedStyle(document.documentElement);
 let textColor = documentStyle.getPropertyValue('--text-color');
 
+const checkboxValue = ref([]);
 const dataviewValue = ref(null);
 const layout = ref('grid');
 const sortKey = ref(null);
@@ -14,6 +20,8 @@ const sortOptions = ref([
     { label: 'Price High to Low', value: '!price' },
     { label: 'Price Low to High', value: 'price' }
 ]);
+
+const poolOptionsModal = ref(null);
 
 const pieOptions = ref({
         plugins: {
@@ -27,19 +35,97 @@ const pieOptions = ref({
         }
     });
 
+const loadingVoteButton = ref(false);
+
+const truncateString = (inputString, maxLength) => {
+    if (inputString.length > maxLength) {
+        return inputString.substring(0, maxLength - 3) + '...';
+    } else {
+    return inputString;
+    }
+}
+
+const vote = async () => {
+    loadingVoteButton.value = true;
+
+    let createVoteListDTO = {
+        userId: 1,
+        voteIdList: checkboxValue.value
+    }
+
+    await createVoteList(createVoteListDTO);
+    loadingVoteButton.value = false
+
+    dataviewValue.value
+    .filter(x => x.id == poolOptionsModal.value.id)
+    .map(x => {
+        let indexes = [];
+
+        checkboxValue.value.forEach((id) => {
+        let index = poolOptionsModal.value.options.findIndex((element) => element.id === id);
+        if (index !== -1) {
+            indexes.push(index);
+        }
+        });
+
+
+        for (let index = 0; index < x.datasets[0].data.length; index++) {
+            if(indexes.includes(index)){
+                x.datasets[0].data[index] ++;
+                x.totalCount++;
+            }
+        }
+
+        return x;
+    })
+
+    display.value = false;
+    checkboxValue.value = [];
+};
+
+const display = ref(false);
+
 const poolhubService = new PoolhubService();
+
+const open = async (poolId) => {
+    let poolOptions = await getPoolOptionListByPostId(poolId);
+    poolOptionsModal.value = poolOptions;
+    display.value = true;
+};
+
+const close = () => {
+    display.value = false;
+};
+
+const openConfirmation = () => {
+    displayConfirmation.value = true;
+};
+
+const closeConfirmation = () => {
+    displayConfirmation.value = false;
+};
+
+const test = () => {
+    console.log("tere");
+};
+
+const stopPropagation = (event) => {
+      event.stopPropagation(); 
+};
 
 onMounted(async () => {
     let res = await poolhubService.getPoolHubList();
-
-    console.log(res);
 
     let formatedPoolList = [];
 
     res.map(pool => {
         let tempData = {
+            id: pool.id,
+            author: pool.author,
             title: pool.title,
-            labels: pool.votes.map(x => x.option),
+            created: pool.created,
+            totalCount: pool.totalCount,
+            labels: pool.votes.map(x => truncateString(x.option, 40)),
             datasets: [
                 {
                     data: pool.votes.map(x => x.count),
@@ -52,86 +138,62 @@ onMounted(async () => {
         formatedPoolList.push(tempData); 
 
     });
- 
-    console.log(formatedPoolList);
 
     dataviewValue.value = formatedPoolList;
 });
 
-// const onSortChange = (event) => {
-//     const value = event.value.value;
-//     const sortValue = event.value;
-
-//     if (value.indexOf('!') === 0) {
-//         sortOrder.value = -1;
-//         sortField.value = value.substring(1, value.length);
-//         sortKey.value = sortValue;
-//     } else {
-//         sortOrder.value = 1;
-//         sortField.value = value;
-//         sortKey.value = sortValue;
-//     }
-// };
 </script>
 
 <template>
+    <Dialog :header="poolOptionsModal && poolOptionsModal.title" v-model:visible="display" :breakpoints="{ '960px': '75vw' }" :style="{ width: '30vw' }" :modal="true">
+        <div class="grid mt-1">
+            <div class="col-12 md:col-12">
+                <span>{{ poolOptionsModal.description }}</span>
+            </div>
+            <div v-for="(poolOption, id) in poolOptionsModal.options" :key="id" class="col-12 md:col-6">
+                <div class="card">
+                    <div class="field-checkbox mb-1">
+                        <Checkbox :id="'checkOption' + id" name="option" :value="poolOption.id" v-model="checkboxValue" />
+                        <label :for="'checkOption' + id">{{poolOption.title}}</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <template #footer>
+            <Button label="Vote" :loading="loadingVoteButton" @click="vote" icon="pi pi-check" class="p-button-outlined"/>
+        </template>
+    </Dialog>
     <div class="grid">
         <div class="col-12">
             <div class="card">
                 <h5>Pool hub</h5>
                 <DataView :value="dataviewValue" :layout="layout" :paginator="true" :rows="9" :sortOrder="sortOrder" :sortField="sortField">
-                    <!-- <template #header>
-                        <div class="grid grid-nogutter">
-                            <div class="col-6 text-left">
-                                <Dropdown v-model="sortKey" :options="sortOptions" optionLabel="label" placeholder="Sort By Price" @change="onSortChange($event)" />
-                            </div>
-                            <div class="col-6 text-right">
-                                <DataViewLayoutOptions v-model="layout" />
-                            </div>
-                        </div>
-                    </template> -->
-                    <!-- <template #list="slotProps">
-                        <div class="col-12">
-                            <div class="flex flex-column md:flex-row align-items-center p-3 w-full">
-                                <img :src="'demo/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="my-4 md:my-0 w-9 md:w-10rem shadow-2 mr-5" />
-                                <div class="flex-1 text-center md:text-left">
-                                    <div class="font-bold text-2xl">{{ slotProps.data.name }}</div>
-                                    <div class="mb-3">{{ slotProps.data.description }}</div>
-                                    <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false" class="mb-2"></Rating>
-                                    <div class="flex align-items-center">
-                                        <i class="pi pi-tag mr-2"></i>
-                                        <span class="font-semibold">{{ slotProps.data.category }}</span>
-                                    </div>
-                                </div>
-                                <div class="flex flex-row md:flex-column justify-content-between w-full md:w-auto align-items-center md:align-items-end mt-5 md:mt-0">
-                                    <span class="text-2xl font-semibold mb-2 align-self-center md:align-self-end">${{ slotProps.data.price }}</span>
-                                    <Button icon="pi pi-shopping-cart" label="Add to Cart" :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'" class="mb-2"></Button>
-                                    <span :class="'product-badge status-' + slotProps.data.inventoryStatus.toLowerCase()">{{ slotProps.data.inventoryStatus }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </template> -->
-
                     <template #grid="slotProps">
                         <div class="col-12 md:col-4">
-                            <div class="card m-3 border-1 surface-border">
-                                <div class="flex align-items-center justify-content-between">
-                                    <!-- <div class="flex align-items-center">
-                                        <i class="pi pi-tag mr-2"></i>
-                                        <span class="font-semibold">{{ slotProps.data.category }}</span>
-                                    </div>
-                                    <span :class="'product-badge status-' + slotProps.data.inventoryStatus.toLowerCase()">{{ slotProps.data.inventoryStatus }}</span> -->
-                                </div>
+                            <div class="card m-2 border-1 surface-border pl-3 pt-3 pr-3 pb-0" :onClick="test" style="cursor: pointer;">
                                 <div class="text-center">
-                                    <div class="text-xl mb-3">{{ slotProps.data.title }}</div>
+                                    <div class="text-xl font-semibold mb-3">{{ slotProps.data.title }}</div>
                                     <Chart type="pie" :data="slotProps.data" :options="pieOptions">{{title}}</Chart>
                                     <div class="mb-3">{{ slotProps.data.description }}</div>
-                                    <!-- <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false"></Rating> -->
                                 </div>
-                                <!-- <div class="flex align-items-center justify-content-between">
-                                    <span class="text-2xl font-semibold">${{ slotProps.data.price }}</span>
-                                    <Button icon="pi pi-shopping-cart" :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"></Button>
-                                </div> -->
+                                <Divider class="mt-0 mr-0 ml-0 mb-2" layout="horizontal" align="center">
+                                    <span class="outline">{{slotProps.data.totalCount}} votes</span>
+                                </Divider>
+                                <div>
+                                    <div class="flex align-items-center justify-content-between">
+                                        <div class="flex align-items-center">
+                                            <Avatar :image="'demo/images/avatar/amyelsner.png'" class="mr" size="large" shape="circle"></Avatar>
+                                            <span class="text-l ml-2">{{ slotProps.data.author.name }}</span>        
+                                        </div>
+                                        <span class="text-l ml-2">{{ new Date(slotProps.data.created).getDate()}}.{{(new Date(slotProps.data.created).getMonth() + 1).toString().padStart(2, '0')}}.{{new Date(slotProps.data.created).getFullYear()}}</span>
+                                    </div>
+                                    <div class="flex align-items-center justify-content-between mt-1" v-if="true">
+                                        <Button class="p-button-rounded p-button-info p-button-text" label="more" icon="pi pi-ellipsis-h" @click.stop="stopPropagation" />
+                                        <Button class="p-button-rounded p-button-info p-button-text" label="share" icon="pi pi-arrow-circle-up" @click.stop="stopPropagation" />
+                                        <Button icon="pi pi-chart-pie" label="vote" class="p-button-rounded p-button-text" @click.stop="stopPropagation" @click="open(slotProps.data.id)"/>
+                                        <!-- <Button class="p-button-rounded p-button-sm" label="" icon="pi pi-chart-pie" style="width: auto" @click.stop="stopPropagation" @click="open(slotProps.data.id)" /> -->
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </template>
